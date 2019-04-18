8f155f2c898107f2661ffb10dd82cfc54025043a
jatin77 <45628336+jatin77@users.noreply.github.com>
2019-04-18 07:08:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+var quotes = [{
+    quote: "Pay attention to the world around you. Chances are you are already incredibly blessed, you just haven’t noticed."
+  },
+  {
+    quote: "“There is no such thing as a hopeless situation. Every single circumstances of your life can change! ”  "
+  },
+  {
+    quote: "There is a truth deep down inside of you that has been waiting for you to discover it, and that truth is this: you deserve all good things life has to offer."
+  },
+  {
+    quote: " You become what you think about most.. But you also attract what you think about most."
+  },
+  {
+    quote: "“Your power is in your thoughts, so stay awake. In other words, remember to remember.” "
+  },
+  {
+    quote: "“Whenever you think you can or think you can’t, either way you are right"
+  },
+  {
+    quote: "Your thoughts become things!"
+  },
+  {
+    quote: "The truth is that the universe has been answering you all of your life, but you cannot receive the answers unless you are awake. "
+  },
+  {
+    quote: "If you are feeling good , it is because you are thinking good thoughts ."
+  },
+  {
+    quote: "Remember that your thoughts are the primary cause of everything"
+  },
+  {
+    quote: "Instead of focusing on the world's problems, give your attention and energy to trust, love, abundance, education and peace"
+  },
+  {
+    quote: "You are the masterpieceof your own life. You are the Michelangelo of your own life.The David you are sculpturing is you"
+  }, {
+    quote: "Ninety-nine percent of who you are is invisible and untouchable."
+  },
+  {
+    quote: "Ask once, believe you have received, and all you have to do to receive is feel good."
+  },
+  {
+    quote: "Every thought of yours is a real thing – a force."
+  },
+  {
+    quote: "Everything else you see and experience in this world is effect, and that includes your feelings. The cause is always your thoughts."
+  },
+  {
+    quote: "See the things that you want as already yours."
+  },
+  {
+    quote: "You are the one who calls the law of attraction into action, and you do it through your thoughts"
+  }
+
+
+];
